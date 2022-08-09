735cbaf26b1f19f9327e14ea7aa9d98479af5dee
Pikalytics <78703078+pikalytics@users.noreply.github.com>
2022-08-09 12:38:24 -0700

    Copy export set link to clipboard

--- dmgcalc/js/ap_calc.js
+++ dmgcalc/js/ap_calc.js
@@ -927,7 +927,27 @@ function exportPokemonLink(playerNum) {
         set.set.moves.push(pokemon.moves[i].name)
     }
     url += window.encodeURI(window.btoa(JSON.stringify(set)))
-    console.log(url)
+
+    // Create an auxiliary hidden input
+    var aux = document.createElement('input')
+
+    // Get the text from the element passed into the input
+    aux.setAttribute(
+        'value',
+        url
+    )
+
+    // Append the aux input to the body
+    document.body.appendChild(aux)
+
+    // Highlight the content
+    aux.select()
+
+    // Execute the copy command
+    document.execCommand('copy')
+
+    // Remove the input from the body
+    document.body.removeChild(aux)
 }
 
 function exportPokemon(playerNum) {
